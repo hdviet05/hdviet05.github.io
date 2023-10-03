bf83aac353a33316db96144a15948938dbdef3a9
Hoang Duc Viet <viethd2704@gmail.com>
2023-10-03 22:37:57 +0700

    changed html into php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="w3.css">
+        <title>Viet's FTU Lecture Notes</title>
+    </head>
+    <body>
+        <header class="w3-container w3-center w3-wide w3-padding-32">
+            <div class="header">
+                <h1><span class='w3-tag'>viet</span> 's FTU lecture notes</h1>
+                <p>my uni notes archive.</p>
+            </div>
+        </header>
+        <main class="w3-container w3-margin-bottom">
+            <!--first blog-->
+            <div class="w3-card w3-hover-shadow w3-container subject"><a href="/subjects/advanced-maths/advanced-maths-main.html"> <!--Add link here-->
+                <h3>Advanced Mathematics</h3>
+                <p class="w3-justify"></p>
+            </div>
+            <br>
+            <!--second blog-->
+            <div class="w3-card w3-hover-shadow w3-container subject"><a href="/subjects/marxism-leninism/marxism-leninism-main.html">
+                <h3>Marxism-Leninism</h3>
+                <p class="w3-justify"></p>
+        </main>
+    </body>
+</html>
